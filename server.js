e5c08466c35382045b995aa3886309dc3cb3ef6e
Karol Bernatowicz <karol.bernatowicz@gmail.com>
2023-11-07 21:05:51 +0100

    Add google authorization

--- server.js
+++ server.js
@@ -2,8 +2,34 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const hbs = require('express-handlebars');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const session = require('express-session');
 
 const app = express();
+
+// configure passport provider options
+passport.use(new GoogleStrategy({
+  clientID: '709561038162-2719ep3mregpiq77toagpja7l85f31ao.apps.googleusercontent.com',
+  clientSecret: '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',
+  callbackURL: 'http://localhost:3030/auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {
+done(null, profile);
+}));
+
+// serialize user when saving to session
+passport.serializeUser((user, serialize) => {
+  serialize(null, user);
+});
+
+// deserialize user when reading from session
+passport.deserializeUser((obj, deserialize) => {
+  deserialize(null, obj);
+});
+
+app.use(session({ secret: 'Eloelo320SyriuszPrime' }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
 app.set('view engine', '.hbs');
@@ -25,10 +51,20 @@ app.get('/user/no-permission', (req, res) => {
   res.render('noPermission');
 });
 
+// Endpoint to google authorization
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['email', 'profile'] }));
+
+app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/user/no-permission' }),
+  (req, res) => {
+    res.redirect('/user/logged');
+  }
+);
+
 app.use('/', (req, res) => {
   res.status(404).render('notFound');
 });
 
-app.listen('8000', () => {
-  console.log('Server is running on port: 8000');
+app.listen('3030', () => {
+  console.log('Server is running on port: 3030');
 });
