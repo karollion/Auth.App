72412e50f5f96f0998aee6259ef4b8b3956833d1
Karol Bernatowicz <karol.bernatowicz@gmail.com>
2023-11-08 18:16:23 +0100

    Move initialize passport and routes to other files

--- server.js
+++ server.js
@@ -3,37 +3,23 @@ const cors = require('cors');
 const path = require('path');
 const hbs = require('express-handlebars');
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const session = require('express-session');
+const passportSetup = require('./config/passport');
 
 const app = express();
 
-// configure passport provider options
-passport.use(new GoogleStrategy({
-  clientID: '709561038162-2719ep3mregpiq77toagpja7l85f31ao.apps.googleusercontent.com',
-  clientSecret: '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',
-  callbackURL: 'http://localhost:3030/auth/google/callback'
-}, (accessToken, refreshToken, profile, done) => {
-done(null, profile);
-}));
-
-// serialize user when saving to session
-passport.serializeUser((user, serialize) => {
-  serialize(null, user);
-});
-
-// deserialize user when reading from session
-passport.deserializeUser((obj, deserialize) => {
-  deserialize(null, obj);
-});
-
-app.use(session({ secret: 'Eloelo320SyriuszPrime' }));
-app.use(passport.initialize());
-app.use(passport.session());
-
+// set handlebars as view engine
 app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
 app.set('view engine', '.hbs');
 
+// init session mechanism
+app.use(session({ secret: 'anything' }));
+
+// init passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// standard middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -43,23 +29,8 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.get('/user/logged', (req, res) => {
-  res.render('logged');
-});
-
-app.get('/user/no-permission', (req, res) => {
-  res.render('noPermission');
-});
-
-// Endpoint to google authorization
-app.get('/auth/google',
-  passport.authenticate('google', { scope: ['email', 'profile'] }));
-
-app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/user/no-permission' }),
-  (req, res) => {
-    res.redirect('/user/logged');
-  }
-);
+app.use('/auth', require('./routes/auth.routes'));
+app.use('/user', require('./routes/user.routes'));
 
 app.use('/', (req, res) => {
   res.status(404).render('notFound');
